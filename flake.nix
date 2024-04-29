d2a3f881c0a130d159e08edb5faf8734f366025d
Carter Ison <carter@isons.org>
2024-04-29 11:16:48 -0700

    Initialize minimal flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+
+inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+outputs = { self, nixpkgs }: let
+  supportedSystems = nixpkgs.lib.systems.flakeExposed;
+  allSystems = output: nixpkgs.lib.genAttrs supportedSystems
+    (system: output nixpkgs.legacyPackages.${system});
+in {
+  packages = allSystems (pkgs: {
+    default = pkgs.rustPlatform.buildRustPackage {
+      pname = "askmod";
+      version = "0.1.0";
+      src = self;
+      cargoLock.lockFile = ./Cargo.lock;
+    };
+  });
+
+  devShells = allSystems (pkgs: {
+    default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        cargo
+        cargo-watch
+        clippy
+      ];
+    };
+  });
+};
+
+}
